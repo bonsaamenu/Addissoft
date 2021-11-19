6af3c663b124910b70363735261ffc04f484265b
Bonsa Amenu <bonsaamenu@gmail.com>
2021-11-19 13:36:59 +0300

    change to card from just div

--- wp-content/themes/wp-bootstrap-starter/template-parts/content-Properties.php
+++ wp-content/themes/wp-bootstrap-starter/template-parts/content-Properties.php
@@ -23,22 +23,32 @@ $title = $image['title'];
 	<section class="bg-light text-dark ">
         <div class="container">
             <div class="d-flex ">
-					<?php if($image):?>
-					<img class="img-fluid w-50" src="<?php echo $pic;?>" alt="<?php echo $alt?>" title="<?php echo $title?>"> 
-					<?php endif;?>
-				<div class="container px-4">
-					<div class="facts">
-						<?php the_field('facts');?>
-					</div>
-					<div class="row ">
-						<div class="bg-secondary col text-light">
-							<?php the_field('price');?>
+				<?php if($image):?>
+				<img class="img-fluid w-50" src="<?php echo $pic;?>" alt="<?php echo $alt?>" title="<?php echo $title?>"> 
+				<?php endif;?>
+				<section >
+					<div class="container">
+						<div class="col">
+							<?php the_field('facts');?>
+						</div>	
+						<div class="row">
+							<div class="col">
+								<div class="card bg-secondary text-light">
+									<div class="card-body">
+										<p class="card-text"><?php the_field('price');?></p>
+									</div>
+								</div>
+							</div>
+							<div class="col">
+								<div class="card bg-info text-dark"  style="width: 15rem;">
+									<div class="card-body">
+										<p class="card-text"><?php the_field('payment_plan');?></p>
+									</div>
+								</div>
+							</div>
 						</div>
-						<div class="bg-info col ">
-							<?php the_field('payment_plan');?>
-						</div>
 					</div>
-				</div>
+					</section>
 			</div>
 		</div>
 	</section>
